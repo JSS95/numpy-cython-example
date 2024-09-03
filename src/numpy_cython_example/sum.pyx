2b15a477246809d28dff54ed834c4c5df2092559
Jisoo Song <jeesoo9595@snu.ac.kr>
2024-09-03 10:51:59 +0900

    fix: make sum_array() more close to C

--- src/numpy_cython_example/sum.pyx
+++ src/numpy_cython_example/sum.pyx
@@ -1,11 +1,13 @@
 import numpy as np
 
+cimport cython
 cimport numpy as cnp
 
 cnp.import_array()
 
-
-def sum_array(cnp.ndarray[cnp.double_t, ndim=1] arr):
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cpdef double sum_array(cnp.ndarray[cnp.double_t, ndim=1] arr):
     cdef Py_ssize_t i
     cdef double total = 0.0
 
